3d58a26aee0ea7eb229e8ad902ed5918c0c63256
Gabtaca <gabrieltaca117@gmail.com>
2024-05-21 10:29:32 -0400

    1st commit

--- Media_query/app.js
+++ Media_query/app.js
@@ -1,33 +1,18 @@
-// Fonction pour changer la couleur de fond en fonction de la taille de l'écran
-function changeBackgroundColor() {
-    if (window.innerWidth < 768) {
-        document.body.style.backgroundColor = 'violet';
-    } else {
-        document.body.style.backgroundColor = 'green';
+
+class User {
+    constructor(fName, lName, age, childrens, isMaried){
+        this.fName - fName;
+        this.lName - lName;
+        this.age - age;
+        this.childrens - childrens;
+        this.isMaried - isMaried;
     }
 }
-
-// Fonction pour ajouter le bouton en fonction de la taille de l'écran
-function addButton() {
-    const button = document.getElementById('responsive-button');
-    if (!button) {
-        const newButton = document.createElement('button');
-        newButton.id = 'responsive-button';
-        newButton.textContent = 'Click Me';
-        if (window.innerWidth >= 1024) {
-            document.body.appendChild(newButton);
-        }
-    }
-}
-
-// Appeler les fonctions au chargement de la page
-window.addEventListener('DOMContentLoaded', () => {
-    changeBackgroundColor();
-    addButton();
-});
-
-// Écouter les événements de redimensionnement de l'écran et réagir en conséquence
-window.addEventListener('resize', () => {
-    changeBackgroundColor();
-    addButton();
-});
+const User = [
+    new User(
+        "michel",
+        "michaud",
+        45,
+        [new User("bobin, lechauve", 12, [], false)],
+    ),
+];
